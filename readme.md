7c6558287f31a4f81a12c98c284bc0a55911a0ac
Steven Vanden Broeck <xytyx@telenet.be>
2016-01-21 15:18:34 +0100

    Init.

---
+++ readme.md
@@ -0,0 +1,4 @@
+# DataAccess.Postgres Toolbox
+
+> Postgres data access for ASP.NET Core 1.0
+
---
+++ src/Toolbox.DataAccess.Postgres/Configuration/PostgresDbConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Toolbox.DataAccess.Postgres
+{
+    public class PostgresDbConfiguration : DbConfiguration
+    {
+        public PostgresDbConfiguration()
+        {
+            SetDefaultConnectionFactory(new Npgsql.NpgsqlConnectionFactory());
+            SetProviderFactory("Npgsql", Npgsql.NpgsqlFactory.Instance);
+            SetProviderServices("Npgsql", Npgsql.NpgsqlServices.Instance);
+        }
+    }
+}
